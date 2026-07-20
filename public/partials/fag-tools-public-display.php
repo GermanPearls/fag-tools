53a67616ce3720e560490f5e14aa82eabdb4c855
GermanPearls <amy@logicallytech.com>
2026-07-20 12:09:53 -0400

    boilerplate file
    Added a public-facing view file for the plugin.

---
+++ public/partials/fag-tools-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://www.logicallytech.com
+ * @since      1.0.0
+ *
+ * @package    Fag_Tools
+ * @subpackage Fag_Tools/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
